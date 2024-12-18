547b8d71166f4fe641515cb14fef5bbb0ede78a0
hwubh <hwubh@connect.ust.hk>
2024-12-18 18:11:55 +0800

    20241218

--- Assets/Script/IK_Solver.cs
+++ Assets/Script/IK_Solver.cs
@@ -8,16 +8,27 @@ public class IK_Solver : MonoBehaviour
     public int m_MaxInteration = 10;
     public float m_Threshold = 1.0f;
 
+
+    private void Update()
+    {
+        CCDIk();
+    }
+
     void CCDIk() 
     {
         for (int i = 0; i < m_MaxInteration; i++) 
         {
             for (int j = 0; j < m_BoneList.Length; j++)
             {
-                Vector3 bone2Effector = m_EffectorPoint.position - m_BoneList[i].position;
-                Vector3 bone2Tartget = m_TargetPoint.position - m_BoneList[i].position;
-                Quaternion rotation = Quaternion.FromToRotation(bone2Effector, bone2Tartget);
-                m_BoneList[i].rotation *= rotation;
+                Vector3 bone2Effector = m_EffectorPoint.position - m_BoneList[j].position;
+                Vector3 bone2Target = m_TargetPoint.position - m_BoneList[j].position;
+                Quaternion rotation = Quaternion.FromToRotation(bone2Effector, bone2Target);
+                Vector3 axis = m_BoneList[j].localToWorldMatrix * Vector3.forward;
+                Debug.DrawLine(m_BoneList[j].position, m_BoneList[j].position + 10 * axis, Color.black);
+                Vector3 projected = Vector3.ProjectOnPlane(bone2Target.normalized, axis);
+                float angle = Vector3.SignedAngle(bone2Effector, projected, axis);
+                Quaternion axis_rotation = Quaternion.AngleAxis(angle, axis);
+                //m_BoneList[j].rotation = rotation * m_BoneList[j].rotation;
             }
             if (Vector3.Distance(m_EffectorPoint.position, m_TargetPoint.position) < m_Threshold)
                 break;
